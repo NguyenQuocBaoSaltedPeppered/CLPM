cb0c257d763a13ab2f4b0eff334fd72df0cac253
ADMIN-PC\Admin <nqbao2712@gmail.com>
2023-10-19 16:54:35 +0700

    gallery UI

---
+++ src/components/button/Button.js
@@ -0,0 +1,12 @@
+import './ButtonStyle.css';
+
+export default function Button({ onClick, children }) {
+    return (
+        <button onClick={e => {
+            e.stopPropagation();
+            onClick();
+        }}>
+            {children}
+        </button>
+    );
+}
---
+++ src/components/profile/Gallery.js
@@ -0,0 +1,12 @@
+import { Profilev2 } from "./Profilev2";
+
+export default function Gallery() {
+    return (
+        <section>
+        <h1>Amazing scientists</h1>
+        <Profilev2 />
+        <Profilev2 />
+        <Profilev2 />
+        </section>
+    );
+}
